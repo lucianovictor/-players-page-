b8c39718ddb8cbf8309fc5891cace22dfbc5e945
Luciano <54756923+lucianovictor@users.noreply.github.com>
2022-04-01 12:13:36 -0300

    :tada: configuring the project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,11 +14,6 @@ export default class MyDocument extends Document {
           <meta property="og:locale" content="pt_BR" />
           <meta property="og:site_name" content="NextJS Boilerplate" />
           <meta name="author" content="Luciano Victor" />
-
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
 
         <body>
